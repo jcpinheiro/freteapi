5aa7e19e50cf2ccc4ac7915ccfe4b9380c84520d
jcpinheiro <jcpinheiro@ifma.edu.br>
2021-04-13 19:09:25 -0300

    Implementação da documentação com swagger

--- src/main/java/edu/ifma/lpweb/freteapi/FreteapiApplication.java
+++ src/main/java/edu/ifma/lpweb/freteapi/FreteapiApplication.java
@@ -3,14 +3,14 @@ package edu.ifma.lpweb.freteapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableAsync;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableCaching
+@EnableSwagger2
 public class FreteapiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FreteapiApplication.class, args);
     }
-
 }
